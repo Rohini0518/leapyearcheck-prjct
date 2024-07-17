c854fc87024e0373561700a105ecdb42d66dab8d
rohini k <108523325+Rohini0518@users.noreply.github.com>
2024-07-17 11:28:45 +0530

    Updated second if condition and ran test  cases, program is working fine now

--- leapyearprjct.py
+++ leapyearprjct.py
@@ -7,7 +7,7 @@ year=int(input("enter any year to check:"))
 
 
 if year%4==0:
-    if year%100!=0 :
+    if year%100=0 :
       if year%400 ==0 :
         print(f"{year} is a leap year ") 
       else:
