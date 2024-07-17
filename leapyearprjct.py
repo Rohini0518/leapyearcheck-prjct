6e0aea625c84434d5d36ed0e8e0a4875a308d611
rohini k <108523325+Rohini0518@users.noreply.github.com>
2024-07-17 11:22:26 +0530

    Updated comments leapyearprjct.py

--- leapyearprjct.py
+++ leapyearprjct.py
@@ -1,6 +1,7 @@
  
 # checking a leap year
-# condition year should divide by 4, and divide by 100 and 400 also then only leap year
+# condition year should divide by 4, and divide by 100 and 400 then all conditions true ,the year is leap year without any doudt.
+# here the condition is If a year is divisible by 4 but not by 100 and not divisible by 400, then it is also a leap year.
 
 year=int(input("enter any year to check:"))
 
